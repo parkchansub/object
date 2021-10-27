146c3b807fc73c75e6646fabb52ae09063d74c09
cs.park <pcs0331@naver.com>
2021-10-27 21:33:57 +0900

    04. 설계 품질과 트레이드오프(P98) 진행

--- src/main/java/com/example/object/theater/Movie.java
+++ src/main/java/com/example/object/theater/Movie.java
@@ -1,14 +1,21 @@
 package com.example.object.theater;
 
+import com.example.object.theater.condition.DiscountCondition;
 import com.example.object.theater.policy.DiscountPolicy;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Movie {
 
     private String title;
     private Duration runningTime;
     private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
     private DiscountPolicy discountPolicy;
 
     public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
